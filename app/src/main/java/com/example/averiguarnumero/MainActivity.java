25402e63cf0bb6c10581e97fd93aeeda652232d1
David Salas <dsf.2528@gmail.com>
2019-10-02 19:28:15 +0200

    intent

--- app/src/main/java/com/example/averiguarnumero/MainActivity.java
+++ app/src/main/java/com/example/averiguarnumero/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.averiguarnumero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,8 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     static int intento = 0;
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,16 +27,31 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
+                String dif;
                 int nIntroducido = Integer.parseInt(editText.getText().toString());
                 if (nIntroducido!=nRandom) {
                     intento++;
-                    textView.setText(intento + " Intentos" +"\n(Solucion: "+nRandom+")");
+                    if (nIntroducido>nRandom){
+                        dif = "mayor";
+                    }else{
+                        dif = "menor";
+                    }
+                    textView.setText(intento + " Intentos" +"\nPista: Tu numero es "+dif);
                     editText.setText("");
                 } else{
-                  textView.setText("VICTORIA");
+                    intento++;
+                    textView.setText("VICTORIA");
                 }
 
             }
         });
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, Main2Activity.class);
+        String message = Integer.toString(intento);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
